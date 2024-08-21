2cc2b4de126b9744dbd7b7892dff57a02637449f
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2024-08-21 18:42:06 +0800

    feat: replace vim-surround with nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,9 +127,20 @@ require("lazy").setup({
         }
     },
 
+    -- {
+    --     'tpope/vim-surround',
+    --     event = on_file_open(),
+    -- },
+
     {
-        'tpope/vim-surround',
-        event = on_file_open(),
+        "kylechui/nvim-surround",
+        -- version = "*",     -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     },
 
     {
