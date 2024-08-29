110413d3a3773b9f08489848dd6ceb9a53854ae2
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2024-08-29 14:30:12 +0800

    feat: replace nvim-code-action-menu to actions-preview.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -244,7 +244,12 @@ require("lazy").setup({
         -- end
     },
 
-    "weilbith/nvim-code-action-menu",
+    {
+        "aznhe21/actions-preview.nvim",
+        config = function()
+            vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+        end,
+    },
 
     {
         'mrcjkb/rustaceanvim',
