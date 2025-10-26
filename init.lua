2ead59e97a880b1157b74ad72919be2fad651d84
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2025-10-26 13:50:53 +0800

    refactor: move telescope keymap

--- init.lua
+++ init.lua
@@ -2,12 +2,6 @@ table.unpack = table.unpack or unpack
 
 require("init")
 
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
-vim.keymap.set("n", "<leader>fp", "<cmd>Telescope frecency<cr>")
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -21,8 +21,8 @@ telescope.setup({
 	defaults = vim.tbl_extend("force", require("telescope.themes").get_ivy(), {}),
 })
 
--- nnoremap <leader>ff <cmd>Telescope find_files<cr>
--- nnoremap <leader>fg <cmd>Telescope live_grep<cr>
--- nnoremap <leader>fb <cmd>Telescope buffers<cr>
--- nnoremap <leader>fh <cmd>Telescope help_tags<cr>
--- nnoremap <leader>fp <cmd>Telescope frecency<cr>
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+vim.keymap.set("n", "<leader>fp", "<cmd>Telescope frecency<cr>")
