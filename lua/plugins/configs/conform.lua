a1ea44fa5bea5d85383006273a63b068e5e26567
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2025-10-29 18:03:46 +0800

    feat: update formatter

--- lua/plugins/configs/conform.lua
+++ lua/plugins/configs/conform.lua
@@ -2,13 +2,14 @@ local prettier = { "prettierd", "prettier", stop_after_first = true }
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		dart = { "dart" },
 		-- Conform will run multiple formatters sequentially
 		-- python = { "isort", "black" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = prettier,
 		typescript = prettier,
-		jsx = prettier,
-		tsx = prettier,
+		javascriptreact = prettier,
+		typescriptreact = prettier,
 		xml = { "xmllint" },
 		rust = { "rustfmt" },
 		toml = { "taplo" },
