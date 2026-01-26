807c9a590e0986563927d5f3de2ac71723a154bb
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2026-01-26 10:43:20 +0800

    feat: gitsigns remove yadm options

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,7 +1,4 @@
 require("gitsigns").setup({
-	-- yadm = {
-	--     enable = true
-	-- },
 	on_attach = function(bufnr)
 		local function map(mode, lhs, rhs, opts)
 			opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
